5690e99bddba6418d2c06774faad82c7787d3576
molly huang <molly.huang@wingwill.com.tw>
2023-02-05 23:44:57 +0800

    add raw navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useMediaQuery from "./hooks/useMediaQuery";
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const isDesktop = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   return (
     <div className="app bg-deep-blue">
       <Navbar
